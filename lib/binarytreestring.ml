e5d0a8f1341889e45924a0ff68fc4758c153f1f5
Anlesvavor <jaredmaster32@gmail.com>
2023-07-13 00:06:57 -0500

    tree_of_string

--- lib/binarytreestring.ml
+++ lib/binarytreestring.ml
@@ -16,18 +16,39 @@ let is_lower letter =
   (code >= (int_of_char 'a')) && (code <= (int_of_char 'z'))
 ;;
 
+let tail = function
+  | [] -> []
+  | _ :: xs -> xs
+;;
+
 let tree_of_string (tree_string : string) =
-  let rec aux = function
-    | ',' :: xs -> (Empty, xs)
-    | x :: ')' :: xs when (is_lower x) -> (Node (x, Empty, Empty), xs)
-    | x :: ',' :: xs when (is_lower x) -> (Node (x, Empty, Empty), xs)
-    | x :: '(' :: xs ->
-      let (l, xs') = aux xs in
-      let (r, xs'') = aux xs' in
-      (Node (x, l, r), xs'')
-    | _ -> (Empty, [])
+  let peel_parenthesis tree_string =
+    tree_string
+    |> tail
+    |> List.rev
+    |> tail
+    |> List.rev
   in
-  aux (List.of_seq (String.to_seq tree_string))
+  let rec parts depth acc str =
+    match str with
+    | [] -> (acc, [])
+    | c :: cs ->
+      match c with
+      | '(' -> parts (succ depth) (c :: acc) cs
+      | ')' -> parts (pred depth) (c :: acc) cs
+      | ',' when (depth = 0) -> (List.rev acc, cs)
+      | _ -> parts depth (c :: acc) cs
+  in
+  let rec aux arr =
+    match arr with
+    | [] -> Empty
+    | x :: xs ->
+      let (root_value, arr) = (x, peel_parenthesis xs) in
+      let (l_string, r_string) = parts 0 [] arr in
+      Node (root_value, aux l_string, aux r_string)
+  in
+  let arr = List.of_seq (String.to_seq tree_string) in
+  aux arr
 ;;
 
 let leaf x = Node (x, Empty, Empty);;

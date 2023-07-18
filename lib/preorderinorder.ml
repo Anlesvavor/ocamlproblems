0bc35fdbfc21f6a3eb3f3afbef613d7e77b26107
Anlesvavor <jaredmaster32@gmail.com>
2023-07-18 00:36:50 -0500

    pre_in_tree; although I don't know why the left hand tree twists...

--- lib/preorderinorder.ml
+++ lib/preorderinorder.ml
@@ -37,3 +37,71 @@ let rec inorder tree =
 ;;
 
 inorder example_layout_tree;;
+
+(* abdecfg *)
+(* dbeacgf *)
+
+let split_l_r_at at list =
+  let rec aux acc list =
+    match list with
+    | [] -> (acc, [])
+    | x :: xs ->
+      if x = at
+      then (acc, xs)
+      else aux (x :: acc) xs
+  in
+  aux [] list
+;;
+
+split_l_r_at 'a' ['d';'b';'e';'a';'c';'g';'f'];;
+
+let rec contains el = function
+  | [] -> false
+  | x :: xs -> if x = el
+    then true
+    else contains el xs
+;;
+
+let pre_in_tree (i : string) (p : string) : (char binary_tree) =
+  let (p : char list) =
+    String.to_seq p
+    |> Seq.fold_left (fun acc x -> x :: acc) []
+    |> List.rev
+  in
+  let (i : char list) =
+    String.to_seq i
+    |> Seq.fold_left (fun acc x -> x :: acc) []
+    |> List.rev
+  in
+  let rec aux (i : char list) (p : char list) : (char binary_tree) =
+    match i with
+    | [] -> Empty
+    | [y] -> Node (y, Empty, Empty)
+    | _ :: _ ->
+      match p with
+      | [] -> Empty
+      | x :: xs ->
+        if contains x i
+        then
+          let (li, ri) = split_l_r_at x i in
+          Node (x, aux li xs, aux ri xs)
+        else
+          aux i xs
+  in
+  aux i p
+;;
+
+
+
+pre_in_tree "dbeacgf" "abdecfg"
+;;
+
+preorder example_layout_tree
+;;
+
+inorder example_layout_tree
+;;
+
+(pre_in_tree (inorder example_layout_tree) (preorder example_layout_tree))
+= example_layout_tree
+;;
